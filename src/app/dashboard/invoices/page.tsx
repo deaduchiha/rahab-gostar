e59305cef33bcf146c1ff29cd005e40b3a7de6a8
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-09-30 22:15:28 +0330

    Refactor invoice display in dashboard to use a table format
    - Replaced the previous invoice rendering with a structured table layout for better readability.
    - Added columns for invoice index, image, description, and date, enhancing the overall presentation of invoice data.
    - Integrated the `Image` component for displaying invoice images with proper sizing and alt text.

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { get_invoices } from "@/api/invoices";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Image from "next/image";
+
 const Page = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: ["invoices"],
@@ -11,16 +21,44 @@ const Page = () => {
     <div>
       <h1 className="text-lg font-bold">فاکتورها</h1>
 
-      {data && (
-        <div className="mt-10 border">
-          {data.map((invoice) => (
-            <div key={invoice.id}>
-              <h1>{invoice.description}</h1>
-              <img src={`/api/r2/${invoice.photo} `} alt="" />
-            </div>
+      <Table className="border mt-8">
+        <TableHeader>
+          <TableRow className="hover:bg-transparent">
+            <TableHead>ردیف</TableHead>
+            <TableHead>عکس</TableHead>
+            <TableHead>توضیحات</TableHead>
+            <TableHead>تاریخ</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data?.map((item, index) => (
+            <TableRow key={item.id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>
+                <Image
+                  className="rounded-lg"
+                  src={`/api/r2/${String(item.photo).trim()}`}
+                  width={52}
+                  height={52}
+                  alt={item.description}
+                />
+              </TableCell>
+              <TableCell className="max-w-10 truncate">
+                {item.description}
+              </TableCell>
+              <TableCell>
+                {new Date(item.createdAt).toLocaleString("fa-IR", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </TableCell>
+            </TableRow>
           ))}
-        </div>
-      )}
+        </TableBody>
+      </Table>
     </div>
   );
 };
