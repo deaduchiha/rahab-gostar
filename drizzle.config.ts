2bf1f4e4becf544ec17063b546503ea188a1ca05
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-09-30 20:13:24 +0330

    Add D1 HTTP driver to drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   schema: "./src/db/schema/**/*.ts",
   out: "./src/db/migrations",
   dialect: "sqlite",
+  driver: "d1-http",
   dbCredentials: {
     accountId: process.env.DB_ACCOUNT_ID || "",
     databaseId: process.env.DB_DATABASE_ID || "",
