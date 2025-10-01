ea66e79980db9ec4ec5e7a3916a7d9c577e2b1b8
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-10-01 16:59:42 +0330

    Add invoice image preview and copy link functionality in dashboard
    - Introduced a dialog component for viewing invoice images in full size.
    - Added a button to copy the image link to the clipboard with a success toast notification.
    - Enhanced the invoice table layout by including an "Add Invoice" button for better user interaction.

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,4 +1,11 @@
 "use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useQuery } from "@tanstack/react-query";
 import { get_invoices } from "@/api/invoices";
 import {
@@ -11,6 +18,9 @@ import {
 } from "@/components/ui/table";
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
+import { Clipboard, PlusIcon } from "lucide-react";
+import { toast } from "sonner";
 
 const Page = () => {
   const { data, isLoading } = useQuery({
@@ -20,7 +30,13 @@ const Page = () => {
 
   return (
     <div>
-      <h1 className="text-lg font-bold">فاکتورها</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-bold">فاکتورها</h1>
+        <Button>
+          <PlusIcon className="w-4 h-4" />
+          افزودن فاکتور
+        </Button>
+      </div>
 
       <Table className="border mt-8">
         <TableHeader>
@@ -29,6 +45,7 @@ const Page = () => {
             <TableHead>عکس</TableHead>
             <TableHead>توضیحات</TableHead>
             <TableHead>تاریخ ایجاد</TableHead>
+            <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -39,12 +56,8 @@ const Page = () => {
               <TableRow key={item.id}>
                 <TableCell>{index + 1}</TableCell>
                 <TableCell className="max-w-12">
-                  <Image
-                    className="rounded-lg w-auto h-auto"
-                    src={`/api/r2/${String(item.photo).trim()}`}
-                    width={52}
-                    height={52}
-                    alt={item.description}
+                  <ImagePreview
+                    image={`/api/r2/${String(item.photo).trim()}`}
                   />
                 </TableCell>
                 <TableCell className="max-w-10 truncate text-xs">
@@ -58,6 +71,26 @@ const Page = () => {
                     hour: "2-digit",
                     minute: "2-digit",
                   })}
+                </TableCell>
+                <TableCell>
+                  <Button
+                    className="bg-amber-500 hover:bg-amber-600 cursor-pointer text-white"
+                    // variant="ghost"
+                    size="icon"
+                    onClick={() => {
+                      navigator.clipboard
+                        .writeText(
+                          `https://rahabgostar.ir/api/r2/${String(
+                            item.photo
+                          ).trim()}`
+                        )
+                        .then(() => {
+                          toast.success("لینک عکس مورد نظر در کلیپبورد کپی شد");
+                        });
+                    }}
+                  >
+                    <Clipboard />
+                  </Button>
                 </TableCell>
               </TableRow>
             ))
@@ -87,4 +120,32 @@ const MockSkeleton = () => {
       </TableCell>
     </TableRow>
   ));
+};
+
+const ImagePreview = ({ image }: { image: string }) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Image
+          className="rounded-lg w-auto h-auto"
+          src={image}
+          width={52}
+          height={52}
+          alt={image.split("/").pop() || "image"}
+        />
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>مشاهده عکس</DialogTitle>
+        </DialogHeader>
+        <Image
+          className="rounded-lg w-full h-auto"
+          src={image}
+          width={1080}
+          height={1080}
+          alt={image.split("/").pop() || "image"}
+        />
+      </DialogContent>
+    </Dialog>
+  );
 };
