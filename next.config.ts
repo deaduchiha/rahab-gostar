639e59cebb080040a99d6274e8d4ea506e4bc139
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-10-01 17:52:16 +0330

    Add remote image pattern for external hosting in Next.js config
    - Configured the `next.config.ts` to allow images from the `rahabgostar.ir` domain by adding a remote pattern under the `images` configuration.
    - Updated import statement for `initOpenNextCloudflareForDev` to maintain consistent formatting.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rahabgostar.ir",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
